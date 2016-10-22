97c9ba2ee3f7df4116aa0bf0432f8d65a1670ff2
Manat Lertpeetipat <mlertpee@gmail.com>
2016-10-23 00:49:15 +0700

    Added #cell_hash to retrieve index more efficient.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,17 @@
 class Board < ApplicationRecord
+  attr_accessor :cell_hash
+
   has_many :cells
 
   validates :name, presence: true
+
+  after_save :generate_cell_hash
+  after_find :generate_cell_hash
+
+
+  private
+
+  def generate_cell_hash
+    self.cell_hash = Hash[cells.map.with_index.to_a]
+  end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Board, type: :model do
   it { is_expected.to have_many(:cells) }
   it { is_expected.to validate_presence_of(:name) }
+
+  subject(:cells) { build_list(:cell, 10, :base) }
+  subject(:board) { create(:board, :base, cells: cells) }
+  it "can tell index of cell in O(1)" do
+    expect(board.cell_hash[cells[4]]).to eq(4)
+  end
 end
