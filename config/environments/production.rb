4dbb6a4a141f1da710f334d378e82eef8d3ef70e
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-24 00:58:51 +0700

    configure redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,9 +37,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-
-  #config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'https://powerful-atoll-32350.herokuapp.com' ]
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://powerful-atoll-32350.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = [ 'https://powerful-atoll-32350.herokuapp.com', 'http://powerful-atoll-32350.herokuapp.com' ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
