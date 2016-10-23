7d400221d94710381fc923ca0db53c69db914cd4
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-23 23:20:42 +0700

    revise path to fix create duplicate player error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :players, only:[:create]
   end
   resources :players, only: [:destroy]
-  post 'players/:id/roll', to: 'players#roll'
+  post 'players/:id/roll', to: 'players#roll', as: 'players_roll'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home/index", to: "home#index", as: "home"
