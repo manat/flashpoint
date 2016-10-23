75476c0b58c161cc0eeed2bb811ce50ce682f849
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-23 17:42:02 +0700

    set root to 'Games' index path

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home/index", to: "home#index", as: "home"
 
-  root to: "home#index"
+  root to: "games#index"
 
 
 end
